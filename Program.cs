3077d67d055a1b48e4003eb168da8840168a2da0
Dongpeng Qian <dongpeng.qian@stmartin.edu>
2024-11-18 09:40:40 -0800

    2nd update

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Rational Project");
+            Rational r1 = new Rational();
+            Rational r2 = new Rational(1, 3);
+            Rational.WriteRational(r1);
+            Rational.WriteRational(r2);
+            r1.WriteRational2(r1);
+            r2.WriteRational2(r2);
         }
     }
+    
 }
---
+++ Rational.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RationalProject
+{
+    internal class Rational
+    {
+        public int numerator, denominator;
+        public Rational()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+        public Rational(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public static void WriteRational(Rational r)
+        {
+            //Console.WriteLine(r);
+            Console.WriteLine($"{r.numerator}/{r.denominator}");
+        }
+        public void WriteRational2(Rational r)
+        {
+            Console.WriteLine($"{r.numerator}/{r.denominator}");
+        }
+        public void Reduce(Rational r)
+        {
+            int gcd = GCD(r.numerator, r.denominator);
+            r.numerator /= gcd;
+            r.denominator /= gcd;
+        }
+        static int GCD(int n1, int n2)
+        {
+            if (n2 == 0)
+            {
+                return n1;
+            }
+            else
+            {
+                return GCD(n2, n1 % n2);
+            }
+        }
+    }
+}
